09e14ef9632f6918276ce400c6eb5c32b37daf41
Damian Nguyen <74862162+aduy6522@users.noreply.github.com>
2025-02-24 15:25:17 +0700

    Update EV_RegistrationFlow_Single.cy.js

--- cypress/e2e/EV_RegistrationFlow_Single.cy.js
+++ cypress/e2e/EV_RegistrationFlow_Single.cy.js
@@ -3,12 +3,7 @@ describe('Event Registration Flow - Single', () => {
 		// Mở trang web hoặc form mà bạn muốn kiểm tra
 		cy.visit('https://evexus-dev.com/event/example/register/246/email');
         cy.wait(5000)
-	cy.get('#rcc-confirm-button', { timeout: 5000 })
-	  .should('exist')
-	  .click()
-	  .catch(() => {
-	    cy.log('Button does not exist, skip this step');
-  });
+	//cy.get('#rcc-confirm-button').click();
 
 	});
 
